48560768e91a88e63d9ae6ab14b989c87a4e3694
DanielArrudas <arrudasmacedo@gmail.com>
2025-08-11 00:51:58 -0300

    Feat: Improves client list display

--- views/ListClients.php
+++ views/ListClients.php
@@ -11,42 +11,59 @@
     <h1>Lista de Clientes</h1>
 
     <div class="content">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>CPF</th>
-                    <th>Telefone</th>
-                    <th>Email</th>
-                    <th>Escolaridade</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if(!empty($resultData)): ?>
-                <?php foreach ($resultData as $data): ?>
-                    <?php
-                    $escolaridadeFormatado = match ($data['escolaridade']) {
-                        1 => 'Ensino Fundamental',
-                        2 => 'Ensino Médio',
-                        3 => 'Ensino Superior',
-                        default => 'Escolaridade Inválida'
-                    }
-                        ?>
+        <?php if (empty($resultData)): ?>
+            <h3>Nenhum cliente cadastrado.</h3>
+        <?php endif; ?>
+
+        <?php if (!empty($resultData)): ?>
+            <table class="table">
+                <thead>
                     <tr>
-                        <td><?= $data['id'] ?></td>
-                        <td><?= $data['nome'] ?></td>
-                        <td><?= $data['cpf'] ?></td>
-                        <td><?= $data['telefone'] ?></td>
-                        <td><?= $data['email'] ?></td>
-                        <td><?= $escolaridadeFormatado ?></td>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>CPF</th>
+                        <th>Telefone</th>
+                        <th>Email</th>
+                        <th>Escolaridade</th>
+                        <th></th>
+                        <th></th>
                     </tr>
-                <?php endforeach; ?>
-            <?php endif; ?>
-            
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($resultData as $data): ?>
+                        <?php
+                        $escolaridadeFormatado = match ($data['escolaridade']) {
+                            1 => 'Ensino Fundamental',
+                            2 => 'Ensino Médio',
+                            3 => 'Ensino Superior',
+                            default => 'Escolaridade Inválida'
+                        }
+                            ?>
+                        <tr>
+                            <td><?= $data['id'] ?></td>
+                            <td><?= $data['nome'] ?></td>
+                            <td><?= $data['cpf'] ?></td>
+                            <td><?= $data['telefone'] ?></td>
+                            <td><?= $data['email'] ?></td>
+                            <td><?= $escolaridadeFormatado ?></td>
+                            <td><a href="">Alterar</a></td>
+                            <td><a href="">Excluir</a></td>
+                        </tr>
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+        <?php endif; ?>
+        <br>
+        <div>
+            <form action="index.php" method="get">
+                <button type="submit" name="action" value="create">Cadastrar Novo Cliente</button>
+            </form>
+        </div>
     </div>
+    <?php if(isset($mensagem)): ?>
+        <p><?= $mensagem ?></p>
+    <?php endif; ?>
 </body>
 
 </html>
