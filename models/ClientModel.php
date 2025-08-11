ded083fcf5e2c801c98902a53336e6162d6e0596
DanielArrudas <arrudasmacedo@gmail.com>
2025-08-11 00:51:16 -0300

    Feat: Implements client data validation and save

--- models/ClientModel.php
+++ models/ClientModel.php
@@ -18,4 +18,52 @@ class ClientModel extends Connect
         $resultQuery = $sqlSelect->fetchAll();
         return $resultQuery;
     }
+    public function isCPFvalide($cpf): bool
+    {
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public function save($formData): array
+    {
+        $erros = [];
+        if (!$this->isCPFvalide($formData['cpf'])) {
+            $erros['cpf'] = 'O CPF informado é inválido.';
+        }
+        if (strlen($formData['telefone']) != 11) {
+            $erros['telefone'] = 'O telefone informado é inválido.';
+        }
+        if (!empty($erros)) {
+            return $erros;
+        }
+        $formData['escolaridade'] = (int) $formData['escolaridade'];
+        $query = "
+        INSERT INTO $this->table (nome, cpf, telefone, email, escolaridade)
+        VALUES (:nome, :cpf, :telefone, :email, :escolaridade);
+        ";
+
+        try {
+            $sqlInsert = $this->connection->prepare($query);
+            $sqlInsert->execute($formData);
+            return [];
+        } catch (PDOException $e) {
+            return ['db_error' => 'Ocorreu um erro ao salvar os dados no servidor.'];
+        }
+    }
 }
