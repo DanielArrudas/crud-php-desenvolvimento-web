f75feab611502b1a5f557be76b0ef9f8d0ceee0c
DanielArrudas <arrudasmacedo@gmail.com>
2025-08-11 00:49:54 -0300

    feat: Sets default PDO fetch mode to associative

--- configuration/connect.php
+++ configuration/connect.php
@@ -15,7 +15,12 @@ class Connect
     public function connectDatabase()
     {
         try {
-            $this->connection = new PDO('mysql:host=' . SERVERNAME . ';dbname=' . DBNAME, USERNAME, PASSWORD);
+            $this->connection = new PDO(
+                'mysql:host=' . SERVERNAME . ';dbname=' . DBNAME,
+                USERNAME,
+                PASSWORD,
+                [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Conexão falhou: " . $e->getMessage();
--- index.php
+++ index.php
@@ -3,6 +3,5 @@
 require_once 'controllers/ClientsController.php';
 
 $action = !empty($_GET['action']) ? $_GET['action'] : 'getAll';
-
 $controller = new ClientsController();
 $controller->{$action}();
