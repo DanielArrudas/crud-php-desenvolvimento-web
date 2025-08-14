472e6d658d439f5ddcd10e73e4e8e87d213cb398
Daniel Arrudas <arrudasmacedo@gmail.com>
2025-08-14 13:10:40 -0300

    Feat: Implements input masking for CPF and phone

--- views/FormEditClient.php
+++ views/FormEditClient.php
@@ -13,19 +13,19 @@
         <h1>Alterar</h1>
         <form action="index.php?action=update&id=<?= $resultClient['id'] ?>" method="post">
             <div>
-                <input type="text" name="nome" class="inputs" placeholder="Digite seu Nome"
+                <input type="text" name="nome" id="nome" class="inputs" placeholder="Digite seu Nome"
                     value="<?= $resultClient['nome'] ?>" required />
             </div>
             <div>
-                <input type="text" name="cpf" class="inputs" placeholder="Digite seu CPF"
+                <input type="text" name="cpf" id="cpf" class="inputs" placeholder="Digite seu CPF"
                     value="<?= $resultClient['cpf'] ?>" required />
             </div>
             <div>
-                <input type="text" name="telefone" class="inputs" placeholder="Digite seu Telefone"
+                <input type="text" name="telefone" id="telefone" class="inputs" placeholder="Digite seu Telefone"
                     value="<?= $resultClient['telefone'] ?>" required />
             </div>
             <div>
-                <input type="email" name="email" class="inputs" placeholder="Digite seu Email"
+                <input type="email" name="email" id="email" class="inputs" placeholder="Digite seu Email"
                     value="<?= $resultClient['email'] ?>" required />
             </div>
 
@@ -56,6 +56,12 @@
             </div>
         <?php endif; ?>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
+    <script>
+        $('#telefone').mask('(00) 00000-0000');
+        $('#cpf').mask('000.000.000-00', { reverse: true });
+    </script>
 </body>
 
 </html>
